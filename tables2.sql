1e1c5257b486a3d28b669485422ae9835b94a72c
Kristian Nilsson <77685746+kristiannilsson@users.noreply.github.com>
2024-11-19 13:27:25 +0100

    Update tables2.sql

--- tables2.sql
+++ tables2.sql
@@ -6,6 +6,7 @@ CREATE TABLE products (
     created_at TIMESTAMP DEFAULT NOW()    -- Default timestamp for when the product was added
 );
 
+
 -- Insert some sample data into the products table
 INSERT INTO products (name, price) VALUES 
 ('Laptop', 999.99),
@@ -14,8 +15,6 @@ INSERT INTO products (name, price) VALUES
 -- Attempting to insert a product with a price of 0 or negative will fail due to CHECK constraint
 INSERT INTO products (name, price) VALUES ('Invalid Product', -10); -- This will raise an error
 
--- Attempting to insert a duplicate SKU will fail due to UNIQUE constraint
-INSERT INTO products (name, price) VALUES ('Tablet', 299.99); -- This will raise an error
 
 -- Create a view to simplify access to the product data
 CREATE VIEW simple_product_overview AS 
@@ -23,7 +22,7 @@ SELECT name, price
 FROM products;
 
 -- Query the view
-SELECT * FROM product_overview;
+SELECT * FROM simple_product_overview;
 
 
 -- Query the products table to see the updated data
@@ -32,7 +31,6 @@ SELECT * FROM products;
 CREATE TABLE users (
     email TEXT PRIMARY KEY,
     username TEXT UNIQUE,  -- Unique constraint. We cant have both as a primary key as then we could have one username with several emails.
-    
 );
 
 /* 
